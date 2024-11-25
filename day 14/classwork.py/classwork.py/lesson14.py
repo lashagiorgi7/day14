1fbec1984a7da3d470eb54e2774771c4835b112f
lashagiorgi7 <lg.muxadgverdeli@gmail.com>
2024-11-25 15:11:11 +0400

    Add files via upload

---
+++ day 14/classwork.py/classwork.py/lesson14.py
@@ -0,0 +1,9 @@
+# user_age = int (input("enter your age "))
+# print(user_age > 14 and user_age < 25)
+
+
+
+
+user_name = input("enter your name" )
+print(user_name == "lasha" or user_name == "giorgi")
+
---
+++ day 14/homework/3homework.py
@@ -0,0 +1,9 @@
+#4)გამოიყენეთ or  რათა გაიგოთ  თუ ცხოველი არის , კატა თუ თაგვი.
+
+
+
+animal = input("enter the name of the animal: ")
+if animal == "cat" or animal == "mouse":
+    print("animal is a cat or  a mouse")
+else:
+    print("the animal is neither a cat or a mouse")
